dfa246d4973b2cf23cc18c1f40fca5ad36df1eaf
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-13 16:46:26 +0530

    backend file updates

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "usecase-3-4-terraform-state"
-    key          = "usecase3-4/terraform.tfstate"
+    key          = "terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
