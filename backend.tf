87e2fb791b25135614b5d373b95033a934e11a02
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-13 15:48:06 +0530

    backend file updates

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "var.s3_bucket_name"
+    bucket       = "usecase-3-4-terraform-state"
     key          = "usecase3-4/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
--- variables.tf
+++ variables.tf
@@ -28,8 +28,3 @@ variable "key_name" {
   type        = string
   
 }
-variable "s3_bucket_name" {
-  description = "The name of the S3 bucket for storing Terraform state"
-  type        = string
-  
-}
